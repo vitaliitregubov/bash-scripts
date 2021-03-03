4c301f7879540f30c03d38f1af6fcc7a14359487
vitalii <tregubovvakharkov@gmail.com>
2021-03-03 21:58:48 +0200

    case statement

--- bashscript.sh
+++ bashscript.sh
@@ -1,13 +1,41 @@
 #! /usr/bin/bash
 
-read -p "Type your password" PASS 
-if [ "$PASS" == "0000" ]
-then
-    echo "welcome"
-elif [ "$PASS" == "" ]
-then
-    echo "empty input!"
-else
-    echo "permission denied"
-fi
+#IF-ELSE STATEMENT
+# read -p "Type your password" PASS 
+# if [ "$PASS" == "0000" ]
+# then
+#     echo "welcome"
+# elif [ "$PASS" == "" ]
+# then
+#     echo "empty input!"
+# else
+#     echo "permission denied"
+# fi
+
+# FILE="file.txt"
+
+# if [ -f file1.txt ]
+# then
+#     echo "it is a file"
+# elif [ -d file1.txt ]
+# then
+#     echo "it is a directory"
+# else
+#     echo "it does not exist"
+# fi
+
+# CASE STATEMENT
+# read -p "are you adult?" ANSWER
+# case "$ANSWER" in
+#     [y/Y] | [yY][eE][sS])
+#         echo "permitted"
+#         ;;
+#     [nN] | [nN][oO])
+#         echo "denied!"
+#         ;;
+#     *)
+#         echo "Invalid input"
+#         ;;
+# esac
+
 
