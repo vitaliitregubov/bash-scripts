3913bc139c7d49285deae548deead2f0ad0ed740
vitalii <tregubovvakharkov@gmail.com>
2021-03-28 11:37:19 +0300

    save lists to LocalStorage

--- script.js
+++ script.js
@@ -4,47 +4,35 @@ const addBtns = document.querySelectorAll('.add-btn:not(.solid)');
 const saveItemBtns = document.querySelectorAll('.solid');
 const addItemContainers = document.querySelectorAll('.add-container');
 const addItems = document.querySelectorAll('.add-item');
-
-const itemLists = document.querySelectorAll('.drag-item-list');
-const backLogList = document.getElementById('backlog-list');
-const progressList = document.getElementById('progress-list');
-const completeList = document.getElementById('complete-list');
-const onHoldList = document.getElementById('on-hold-list');
+const listOfColumns = document.querySelectorAll('.drag-item-list');
 
 let updatedOnLoad = false;
 let draggedItem;
+let currentColumn;
 
-let backLogListArray = [];
-let progressListArray = [];
-let completeListArray = [];
-let onHoldListArray = [];
-let listArrays = [];
+const data = {
+  backLogList: [],
+  progressList: [],
+  completeList: [],
+  onHoldList: []
+};
 
 function getSavedColumns() {
-  if (localStorage.getItem('backlogItems')) {
-    backLogListArray = JSON.parse(localStorage.backlogItems);
-    progressListArray = JSON.parse(localStorage.progressItems);
-    completeListArray = JSON.parse(localStorage.completeItems);
-    onHoldListArray = JSON.parse(localStorage.onHoldItems);
+  if (localStorage.getItem('backLogList')) {
+    for(const column in data) {
+      data[column] = JSON.parse(localStorage[column])
+    }
   } else {
-    backLogListArray = ['Work on projects', 'Do something...'];
-    progressListArray = ['Work on projects', 'Do something...'];
-    completeListArray = ['Work on projects', 'Do something...'];
-    onHoldListArray = ['Do something...'];
+    for(const column in data) {
+      data[column] = ['Work on projects', 'Get some rest', 'Go on working']
+    }
   }
 }
 
 function updateSavedColumns() {
-  const arrays = [
-    { array: backLogListArray, name: 'backlogItems' },
-    { array: progressListArray, name: 'progressItems' },
-    { array: completeListArray, name: 'completeItems' },
-    { array: onHoldListArray, name: 'onHoldItems' }
-  ];
-
-  arrays.forEach(item => {
-    localStorage.setItem(item.name, JSON.stringify(item.array))
-  })
+  for(const column in data) {
+    localStorage.setItem(column, JSON.stringify(data[column]))
+  }
 }
 
 function createEl(columnEl, column, item, index) {
@@ -65,31 +53,35 @@ function updateDOM() {
     getSavedColumns();
   }
 
-  backLogList.textContent = '';
-  backLogListArray.forEach((item, index) => {
-    createEl(backLogList, 0, item, index)
-  });
+  for(const column in data) {
+    const ul = document.getElementById(column);
+    ul.textContent = "";
+    data[column].forEach((item, index) => {
+      createEl(ul, 0, item, index)
+    })
+  }
 
-  progressList.textContent = '';
-  progressListArray.forEach((item, index) => {
-    createEl(progressList, 0, item, index)
-  });
+  updatedOnLoad = true;
+  updateSavedColumns();
+}
 
-  completeList.textContent = '';
-  completeListArray.forEach((item, index) => {
-    createEl(completeList, 0, item, index)
-  });
+//  Update arrays after dropping an item
+function updateArrays() {
+  listOfColumns.forEach(({ id }) => {
+    data[id] = [];
 
-  onHoldList.textContent = '';
-  onHoldListArray.forEach((item, index) => {
-    createEl(onHoldList, 0, item, index)
-  });
+    const children = document.getElementById(id).children
+    for(let i = 0; i < children.length; i++) {
+      data[id].push(children[i].textContent);
+    }
+  })
+  
+  updateDOM();
 }
 
 // Drag func
 function drag(event) {
   draggedItem = event.target;
-  console.log(draggedItem)
 }
 
 function allowDrop(event) {
@@ -97,10 +89,19 @@ function allowDrop(event) {
 }
 
 function dragEnter(column) {
-  console.log(column)
+  document.getElementById(column).classList.add('over');
+  currentColumn = column;
 }
 
 // Dropping item in a column
-function drop(event) {
-  event.preventDefault();
+function drop(id) {
+  listOfColumns.forEach(column => {
+    column.classList.remove('over');
+
+    if(column.id === id) {
+      column.appendChild(draggedItem)
+    }
+  })
+
+  updateArrays();
 }
